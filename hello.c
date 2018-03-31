d8722ae04133982ecd9797587e4a5c0a7f1a29db
Sriram Sami <frizensami@gmail.com>
2018-03-31 14:06:55 +0800

    Add who parameter to hello

--- hello.c
+++ hello.c
@@ -2,9 +2,15 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
+
+static char *who = "Sriram";
+module_param(who, charp, 0000); 
+MODULE_PARM_DESC(who, "The module will say \"hello <who>\" at the start!"); 
+
 static int hello_init(void)
 {
 printk(KERN_ALERT "Hello, world\n");
+printk(KERN_ALERT "hello %s\n", who);
 return 0;
 }
 static void hello_exit(void)
